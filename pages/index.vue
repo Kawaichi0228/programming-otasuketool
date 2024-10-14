8abaf30b79d75c6a15cebb0c7ac083404dba10d4
kawaichi0228 <kawaichi513bdxa8ga@gmail.com>
2024-10-14 16:43:43 +0900

    feat: UIをおしゃれにした

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,13 @@
 
     <div class="mb-8">
       <div v-for="(item, index) in troubleshootingItems" :key="index" class="mb-6">
-        <p class="font-medium mb-2">{{ item.question }}</p>
+        <div class="flex mb-2 gap-4">
+          <div
+            class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-500 text-white font-bold text-sm mb-2">
+            {{ item.questionNumber }}
+          </div>
+        </div>
+        <p class="mb-2 font-bold text-gray-600">{{ item.question }}</p>
         <div class="flex space-x-4">
           <button v-for="option in item.options" :key="option" class="font-bold py-2 px-4 rounded" :class="[
         item.selected === option
@@ -14,6 +20,7 @@
             {{ option }}
           </button>
         </div>
+        <div class="border-b border-gray-300 w-full mt-8"></div>
       </div>
     </div>
 
@@ -37,42 +44,50 @@ const router = useRouter();
 
 const troubleshootingItems = ref([
   {
-    question: '1. ローカルサーバー(npm run dev、yarn devなど)の再起動・Dockerの再起動・ブラウザの更新・ファイル上書き保存して再度試しましたか？(そもそも許可されているか？すぐにそれが完了できるか？)',
+    questionNumber: 1,
+    question: 'ローカルサーバー(npm run dev、yarn devなど)の再起動・Dockerの再起動・ブラウザの更新・ファイル上書き保存して再度試しましたか？(そもそも許可されているか？すぐにそれが完了できるか？)',
     options: ['試した', '試せる状況でない'],
     selected: null
   },
   {
-    question: '2. エラーのログが出ているか？また、ログを出す方法は分かっているか？(ブラウザの検証ツール、VSCodeのターミナル、Dockerコンテナの中 など)',
+    questionNumber: 2,
+    question: 'エラーのログが出ているか？また、ログを出す方法は分かっているか？(ブラウザの検証ツール、VSCodeのターミナル、Dockerコンテナの中 など)',
     options: ['ログは出ている', 'ログが出ていない'],
     selected: null
   },
   {
-    question: '3. そのログから・エラーが発生しているファイルの場所(できれば行数も)・エラーの内容の2つが分かるか？',
+    questionNumber: 3,
+    question: 'そのログから・エラーが発生しているファイルの場所(できれば行数も)・エラーの内容の2つが分かるか？',
     options: ['2つとも分かった', '分かっていない'],
     selected: null
   },
   {
-    question: '4. エラーの内容をコピーし、検索エンジンでの検索およびAIへの質問を行い、それを試したか？(Google検索、ChatGPT など)',
+    questionNumber: 4,
+    question: 'エラーの内容をコピーし、検索エンジンでの検索およびAIへの質問を行い、それを試したか？(Google検索、ChatGPT など)',
     options: ['検索・質問したうえで試した', 'これからする'],
     selected: null
   },
   {
-    question: '5. コードのエラー部分だけ、既存プロジェクト内の似たような機能・公式ドキュメントのサンプル・生成AIでの出力等でのコピーで、1から作り直すことは可能か？かつそれを試したか？(現時点のコードの方向性が、要件の実現に対して、あまり好ましくない方法で行おうとしている可能性があるため)',
+    questionNumber: 5,
+    question: 'コードのエラー部分だけ、既存プロジェクト内の似たような機能・公式ドキュメントのサンプル・生成AIでの出力等でのコピーで、1から作り直すことは可能か？かつそれを試したか？(現時点のコードの方向性が、要件の実現に対して、あまり好ましくない方法で行おうとしている可能性があるため)',
     options: ['エラー部分のみ作り直したかつ試した', '作り直す影響範囲が多くできない'],
     selected: null
   },
   {
-    question: '6. issueや公式サポートフォーラムなどに質問を行い、それを試したか？(GitHub Issue、Stack Overflow など)',
+    questionNumber: 6,
+    question: 'issueや公式サポートフォーラムなどに質問を行い、それを試したか？(GitHub Issue、Stack Overflow など)',
     options: ['質問したうえで試した', 'これからする'],
     selected: null
   },
   {
-    question: '7. そもそも元の要件・設計が実現可能なものであるか見直したか？また、今やろうとしている条件に必ずしもこだわらなくてもよいのでは？(別のパッケージに変更が許されるのであればそちらを使ったほうが早い など)',
+    questionNumber: 7,
+    question: 'そもそも元の要件・設計が実現可能なものであるか見直したか？また、今やろうとしている条件に必ずしもこだわらなくてもよいのでは？(別のパッケージに変更が許されるのであればそちらを使ったほうが早い など)',
     options: ['見直したが適切であり、別の方法は許されない', '見直した結果、別の方法が許された'],
     selected: null
   },
   {
-    question: '8. ここまできたら、迷わずすぐに上司やチームメンバーに相談しましょう！！！(コードレビュー、ペアプログラミング など)',
+    questionNumber: 8,
+    question: 'ここまできたら、迷わずすぐに上司やチームメンバーに相談しましょう！！！(コードレビュー、ペアプログラミング など)',
     options: ['相談した', 'これからする'],
     selected: null
   }
